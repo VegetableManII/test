62fdd46b0a1fe9320f22d793528ce79fac8e5178
VegetableManII <1343739480@qq.com>
2021-03-29 23:57:59 +0800

    "update"

--- go模块体验/defer体验/defer.go
+++ go模块体验/defer体验/defer.go
@@ -3,10 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	if true {
-		defer fmt.Printf("1")
-	} else {
-		defer fmt.Printf("2")
+	res := Inc()
+	fmt.Println(res)
+}
+func Inc() (v int) {
+	defer func() { v++ }()
+	return 42
+	// return 执行了三个操作
+	// 1. 将v的值赋值42
+	// 2. 执行defer
+	// 3. 执行RET指令
+}
+func function() {
+	for i := 0; i < 3; i++ {
+		defer func() { println(i) }()
 	}
-	fmt.Printf("3")
 }

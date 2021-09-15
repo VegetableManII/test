d8985e106ab0fd6c2ded1df89f5e1befb4d30484
VegetableManII <1343739480@qq.com>
2021-09-15 11:49:57 +0800

    update

--- Test/redis/main.go
+++ Test/redis/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"time"
 
@@ -12,16 +13,37 @@ local res = redis.call('get',KEYS[1])
 redis.call('expire',KEYS[1],ARGV[1])
 return res`
 
-var lua *redis.Script
+var luas map[string]*redis.Script
+
+type RedisConnSt struct {
+	conn   redis.Conn
+	prefix string
+}
 
 func init() {
-	lua = redis.NewScript(1, script)
+	luas = make(map[string]*redis.Script)
+	scriptStr := map[string]string{
+		"s1": `
+		if ( redis.call('ttl',KEYS[1]) <= -1 )  -- [ 如果模板不存在或未设置ttl ]
+		then
+			redis.call('incr',KEYS[1])
+			redis.call('expire',KEYS[1],ARGV[1])
+		else
+			redis.call('incr',KEYS[1])
+		end
+		`,
+		"s2": `
+			local res = redis.call('get',KEYS[1])
+			redis.call('expire',KEYS[1],ARGV[1])
+			return res
+		`,
+	}
+	for k, v := range scriptStr {
+		luas[k] = redis.NewScript(-1, v)
+	}
 }
 
 func main() {
-	//lua = redis.NewScript(1, script)
-
-	lua1 := redis.NewScript(1, script)
 	c, e := redis.Dial("tcp", "127.0.0.1:6379",
 		redis.DialClientName("test"),
 		redis.DialConnectTimeout(1*time.Second),
@@ -31,11 +53,41 @@ func main() {
 	if e != nil {
 		log.Fatalln(e)
 	}
-	lua1.Load(c)
-	args := redis.Args{}.Add(interface{}("haha"), interface{}(10))
-	reply, err := redis.String(lua.Do(c, args...))
-	if err != nil {
-		log.Println("err", err)
+	rc := RedisConnSt{
+		conn:   c,
+		prefix: "jojo",
 	}
-	log.Println("replay", reply)
+	value, err := rc.EvalScript("s1", 1, "dio", 600)
+	if err == redis.ErrNil {
+		log.Println(err)
+	}
+	log.Println(value)
+}
+
+// ExecScript 参数，numkey为脚本中使用到的KEY的数量，argvs为脚本中的所有的KEY和ARGV
+// 要求所有的KEY排在所有的ARGV前面，并且KEY与ARGV对应，所有的KEY都为字符串类型
+// eg:
+//   ExecScript("script1",numkeys,key1,key2 [key...],arg [arg...])
+//   ExecScript("script2",numkeys,key1,key2 [key...],arg [arg...])
+func (rc *RedisConnSt) EvalScript(script string, numkey int, argvs ...interface{}) (string, error) {
+	if _, ok := luas[script]; !ok {
+		return "", nil
+	}
+	lua := luas[script]
+	lua.Load(rc.conn)
+	for i := 0; i < numkey; i++ {
+		if s, ok := argvs[i].(string); !ok {
+			return "", errors.New("KEYS type assert is not string")
+		} else {
+			argvs[i] = rc.prefix + s
+		}
+	}
+	rargs := []interface{}{}
+	rargs = append(rargs, numkey)
+	rargs = append(rargs, argvs...)
+	value, err := redis.String(lua.Do(rc.conn, rargs...))
+	if err != nil {
+		return "", err
+	}
+	return value, nil
 }

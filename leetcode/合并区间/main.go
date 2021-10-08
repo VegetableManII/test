f354b732ed8c4ff202baaafb19dde8053cd91601
VegetableManII <1343739480@qq.com>
2021-10-08 11:42:39 +0800

    update

--- leetcode/合并区间/main.go
+++ leetcode/合并区间/main.go
@@ -11,7 +11,7 @@ type Interval struct {
 }
 type IntervalSlice []*Interval
 
-func main() {
+func main1() {
 	res := merge([]*Interval{
 		{2, 3}, {2, 2}, {3, 3}, {1, 3}, {5, 7}, {2, 2}, {4, 6},
 	})

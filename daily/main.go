de98ae01157abe0d9ad4dd182b9ded347b4e31a3
VegetableManII <1343739480@qq.com>
2021-11-23 11:47:03 +0800

    update

--- daily/main.go
+++ daily/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "study/daily/questions"
+import (
+	"fmt"
+)
+
+func f1(data ...interface{}) {
+	fmt.Println(data...)
+}
+
+func f2() func() int {
+	a, b := 0, 1
+	return func() int {
+		tmp := a + b
+		a, b = b, tmp
+		return tmp
+	}
+}
 
 func main() {
-	questions.PrintPointerStu()
+	// var array []interface{} = []interface{}{0, 1, 2, 3, 4}
+	// f1(array)
+	// f1(array...)
+
+	// f := f2()
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Printf("%d ", f())
+	// }
+	select {}
 }

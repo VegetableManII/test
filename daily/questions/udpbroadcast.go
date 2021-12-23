525d7a6ddf24be673a592ca55f66aa380e5f8238
VegetableManII <1343739480@qq.com>
2021-12-23 17:51:47 +0800

    update

--- daily/questions/udpbroadcast.go
+++ daily/questions/udpbroadcast.go
@@ -1,10 +1,13 @@
 package questions
 
 import (
-	"bytes"
+	"fmt"
 	"log"
 	"net"
+	"time"
 )
+
+// '0' = 48 = 0x30
 
 // 广播服务端接收消息
 func UdpBroadcastReceive() {
@@ -17,27 +20,46 @@ func UdpBroadcastReceive() {
 	}
 	log.Println("listen on", l.LocalAddr().String())
 
-	data := make([]byte, 512)
+	data := make([]byte, 1024)
 	for {
 		n, remote, e := l.ReadFromUDP(data)
 		if e != nil {
 			log.Panicln(e)
 		}
 		if remote != nil {
-			log.Println("client", remote, n)
+			log.Println("remote", remote, n)
 			log.Println("msg", string(data))
+			text := ""
+			fmt.Scanln(&text)
+			if e != nil {
+				log.Panicln(e)
+			}
+			log.Println(text)
+			_, e = l.WriteToUDP(stringToBytes(text), remote)
+			if e != nil {
+				log.Panicln(e)
+			}
+		} else {
+			time.Sleep(5 * time.Second)
 		}
+		data = data[:0] // 清空缓冲区
 	}
 }
 
 var msg = []byte{0x01, 0x01, 0x00, 0x00, 0x12, 0x34, 0x56, 0x78}
 
+func stringToBytes(s string) []byte {
+	b := make([]byte, 0, len(s))
+	for i := 0; i < len(s); i++ {
+		b = append(b, s[i]-0x30)
+	}
+	return b
+}
+
 // 广播客户端主动发送广播消息
 func UdpBroadcastSend() {
-	buf := make([]byte, 1024)
-	buffer := bytes.NewBuffer(buf)
-
-	la, err := net.ResolveUDPAddr("udp4", "192.168.12.151:12345")
+	buf := make([]byte, 0, 1024)
+	la, err := net.ResolveUDPAddr("udp4", ":11111")
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -51,22 +73,16 @@ func UdpBroadcastSend() {
 	}
 
 	conn.Write([]byte("start work"))
-	// reader := bufio.NewReader(os.Stdin)
-	// text, err := reader.ReadBytes('\n')
 	for {
-		buffer.Reset()
-		_, _, err = conn.ReadFromUDP(buffer.Bytes())
+		_, _, err = conn.ReadFromUDP(buf)
 		if err != nil {
 			log.Panicln(err)
 		}
-		log.Println(buffer.String())
-		buffer.Reset()
-		buffer.Write(msg)
-		_, err := conn.Write(buffer.Bytes())
+		log.Println(string(buf))
+		_, err := conn.Write(msg)
 		if err != nil {
 			log.Panicln(err)
 		}
-		//reader.Reset(os.Stdin)
-
+		buf = buf[:0]
 	}
 }
